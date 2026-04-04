6d2c40fad5589bdf969b3701567d583f8266bc90
Nitya Shah <ncs110905@gmail.com>
2026-04-04 16:34:03 +0530

    Success

--- Insight/settings.py
+++ Insight/settings.py
@@ -134,8 +134,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'e6564064@gmail.com'
-EMAIL_HOST_PASSWORD = 'ysmk wgyh qzex vxcb'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
