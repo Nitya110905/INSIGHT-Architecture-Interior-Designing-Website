b1784b388bad9c0fed747d8dd778e8acfa3a736f
Nitya Shah <ncs110905@gmail.com>
2026-04-06 12:22:11 +0530

    Switched to SSL port 465 to prevent Render timeout

--- Insight/settings.py
+++ Insight/settings.py
@@ -133,10 +133,12 @@ SESSION_SAVE_EVERY_REQUEST = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
+EMAIL_TIMEOUT = 10
 load_dotenv()
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
