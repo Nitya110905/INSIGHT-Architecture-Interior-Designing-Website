32793bbf4c4c87856d679a5f227bc7f4db556073
Nitya Shah <ncs110905@gmail.com>
2026-04-07 15:12:59 +0530

    Final Set-Up

--- Insight/settings.py
+++ Insight/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / 'myapp' / 'static',
 ]
 
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
