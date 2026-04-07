4ec5c6b707631240d0e3052a0254c8fa774847fc
Nitya Shah <ncs110905@gmail.com>
2026-04-07 15:30:53 +0530

    Add debug logs to find missing static files

--- build.sh
+++ build.sh
@@ -4,5 +4,15 @@ set -o errexit
 
 pip install --upgrade pip setuptools wheel
 pip install -r requirements.txt
+
+# --- THE DEBUG HACK ---
+echo "👀 DEBUG: WHAT FOLDERS ARE HERE?"
+ls -la
+echo "👀 DEBUG: WHAT IS INSIDE MYAPP?"
+ls -la myapp/
+echo "👀 DEBUG: WHERE ARE THE STATIC FOLDERS?"
+find . -type d -name "static"
+# ----------------------
+
 python manage.py collectstatic --no-input
 python manage.py migrate
